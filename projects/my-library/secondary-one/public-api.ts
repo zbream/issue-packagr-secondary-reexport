2793d8f6c1753ab795b76dc4baa2a40b22d066ab
Zack Ream <Zack.Ream@hyland.com>
2021-03-12 22:01:51 -0500

    feat: add success case

---
+++ projects/my-library/secondary-one/public-api.ts
@@ -0,0 +1,1 @@
+export * from './lib/secondary-one.service';
--- projects/my-library/src/public-api.ts
+++ projects/my-library/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of my-library
  */
 
-export * from './lib/my-library.service';
-export * from './lib/my-library.component';
-export * from './lib/my-library.module';
+export * from 'my-library/secondary-one';
+export * from 'my-library/secondary-two';
